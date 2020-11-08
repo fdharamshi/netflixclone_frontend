eee1aefd26647e9df348e189e791922466652a65
fdharamshi <femin.dharamshi@sakec.ac.in>
2020-11-09 02:24:26 +0530

    Row component used for all categories

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
-import Row from './Row';
-import requests from './requests';
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
 
 // TBDb API Key : 1fd53e386944e3f69145a25aab0e1e89
 // https://netflixclone-83782.web.app/
@@ -9,8 +9,17 @@ function App() {
   return (
     <div className="App">
       <h1>Femin Dharamshi</h1>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
