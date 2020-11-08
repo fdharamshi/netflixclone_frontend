7e3b6561d0b72fb3bdcda7ffe628fd1ea3643e8e
fdharamshi <femin.dharamshi@sakec.ac.in>
2020-11-09 01:54:48 +0530

    Base structure created. Row component added. Displays title and fetches images.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Row from './Row';
+import requests from './requests';
+
+// TBDb API Key : 1fd53e386944e3f69145a25aab0e1e89
+// https://netflixclone-83782.web.app/
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Femin Dharamshi</h1>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
     </div>
   );
 }
---
+++ src/Row.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
+
+const base_url = "https://image.tmdb.org/t/p/original/"
+
+function Row({ title, fetchUrl}) {
+
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        // if [], run once when the row loads, and don't run it again.
+
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            // console.log(request.data.results);
+            setMovies(request.data.results);
+            return request;
+        }
+
+        fetchData();
+
+    }, [fetchUrl]);
+
+    return (
+        <div className="row">
+            <h2>{title}</h2>
+
+            <div className="row__posters">
+                {/* {several row__poster(s) } */}
+
+                {movies.map(movie => (
+                    <img className="row__poster" src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+                ))}
+            </div>
+            {/* container -> posters */}
+        </div>
+    )
+}
+
+export default Row;
