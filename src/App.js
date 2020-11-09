ed1218424c86622db40b45b2ef7a14432f79d7c9
fdharamshi <femin.dharamshi@sakec.ac.in>
2020-11-09 10:49:07 +0530

    Banner component added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
+import Banner from "./Banner";
 
 // TBDb API Key : 1fd53e386944e3f69145a25aab0e1e89
 // https://netflixclone-83782.web.app/
@@ -8,10 +9,11 @@ import requests from "./requests";
 function App() {
   return (
     <div className="App">
-      <h1>Femin Dharamshi</h1>
+      <Banner />
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchNetflixOriginals} />
