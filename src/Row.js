6ce9d500fa64f1958b2e1ff604ca589acaf121d9
fdharamshi <femin.dharamshi@sakec.ac.in>
2020-11-09 12:15:58 +0530

    Nav finished

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
+import Youtube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     // if [], run once when the row loads, and don't run it again.
@@ -20,17 +23,38 @@ function Row({ title, fetchUrl, isLargeRow }) {
     fetchData();
   }, [fetchUrl]);
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if(trailerUrl){
+      setTrailerUrl("");
+    }else{
+      movieTrailer(movie?.name || "")
+      .then(url => {
+        const urlParams = new URLSearchParams(new URL(url).search);
+        setTrailerUrl(urlParams.get("v"));
+        // console.log(`search result: ${urlParams.get("v")}`);
+      }).catch((error) => console.log(error));
+    }
+  }
+
   return (
     <div className="row">
       <h2>{title}</h2>
 
       <div className="row__posters">
-        {/* {several row__poster(s) } */}
 
         {movies.map((movie) => (
           <img
             key={movie.id}
-            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            // onClick={() => handleClick(movie)}
+            className={`row__poster ${(isLargeRow && "row__posterLarge")}`}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
             }`}
@@ -38,7 +62,8 @@ function Row({ title, fetchUrl, isLargeRow }) {
           />
         ))}
       </div>
-      {/* container -> posters */}
+
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
